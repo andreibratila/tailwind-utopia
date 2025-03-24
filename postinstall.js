d52ac9ae8cc16ff948b4518f214c642b2ec2880b
Andrei Florian Bratila <andreiflorianbratila@gmail.com>
2025-03-24 13:28:40 +0100

    Mod Warning --save

---
+++ postinstall.js
@@ -0,0 +1,6 @@
+if (!process.env.npm_config_dev) {
+  console.log(
+    "\x1b[33m%s\x1b[0m",
+    "⚠️  It is recommended to install this package as a devDependency: npm install --save-dev @andreibratila/tailwind-utopia"
+  );
+}
