0c12b65c3d281bd53fde9297a668b5caa5b17f4e
Andrei Florian Bratila <andreiflorianbratila@gmail.com>
2025-03-24 13:51:16 +0100

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+import path from "path";
+import fs from "fs/promises";
 
 const packageJsonPath = path.join(process.cwd(), "package.json");
 
